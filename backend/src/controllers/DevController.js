b973da124670392cf02ad366d816139542e67a3c
Eduardo Godoy <eduardo.godoy@agenciaeconverse.com.br>
2020-01-17 23:44:23 -0300

    Feature: Implementing Front-end Web - Day 05

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 // index(Mostrar uma lista), show(Um único), store(Criar), update, destroy
 
@@ -36,6 +37,16 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
+
+            // Filtrando as conexões que estão há no máximo 10km de distância e
+            // que o novo dev tenha pelo menos 1 das tecnologias filtradas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack123@cluster0-gizmd.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
